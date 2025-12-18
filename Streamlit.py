fce955e5cbbcc03bb9466bd1e6b9942def19b6db
zac1227 <a0986710233@gmail.com>
2025-12-18 13:34:15 +0800

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -86,7 +86,7 @@ def run_cvd_demo_page():
     x_train = x.drop(columns=["Y"])
 
     
-    NIHSS = st.sidebar.number_input("NIHSS", min_value=0, value=1, step=1)
+    NIHSS = st.sidebar.number_input("NIHSS", min_value=0,max_value=42, value=1, step=1)
     HR_Max = st.sidebar.number_input("HR_Max", min_value=0, value=108, step=1)
     BT_Mean = st.sidebar.number_input("BT_Mean", min_value=0.0, value=37.1, step=0.1,
     format="%.1f")
